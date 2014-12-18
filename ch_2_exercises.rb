42f8978d47ae9a5a230a954488e4ef8737e34012
jakekaad <kaadalac@gmail.com>
2014-12-18 21:05:14 +0000

    Finished OOP exercises

--- ch_2_exercises.rb
+++ ch_2_exercises.rb
@@ -1,10 +1,35 @@
-class Person
-  attr_accessor :name
-  def initialize(name)
+# class Person
+#   attr_accessor :name
+#   def initialize(name)
+#     @name = name
+#   end
+# end
+
+# bob = Person.new("Steve")
+# bob.name = "Bob"
+# puts bob.name
+
+class Student
+  
+  def initialize(name, grade)
     @name = name
+    @grade = grade
   end
+  
+  def better_grade_than?(other_student)
+    grade > other_student.grade
+  end
+  
+  
+  protected
+  
+    def grade
+      @grade
+    end
+      
+  
 end
 
-bob = Person.new("Steve")
-bob.name = "Bob"
-puts bob.name
+joe = Student.new("Joe", 99)
+bob = Student.new("Bob", 90)
+puts "Well done!" if joe.better_grade_than?(bob)
--- my_car.rb
+++ my_car.rb
@@ -1,17 +1,29 @@
-class MyCar
-attr_accessor  :color, :model, :speed
+module Haulable
+  
+  def haul_a_load
+    puts "I am hauling a load"
+  end
+  
+end
+
+
+class Vehicle
+attr_accessor :color, :model, :speed
 attr_reader :year
+@@number_of_vehicles = 0
+
 
   def initialize(y, c, m)
-    @year = y 
-    @color = c 
+    @year = y
+    @color = c
     @model = m
     @speed = 0
+    @@number_of_vehicles += 1
   end
   
   def speed_up(i)
     self.speed += i
-    self.speed_of_car
+    self.speed_of_vehicle
   end
   
   def slow_down(i)
@@ -19,16 +31,28 @@ attr_reader :year
     if self.speed < 0
       self.speed = 0
     end
-    self.speed_of_car
+    self.speed_of_vehicle
   end
   
-  def speed_of_car
+  def speed_of_vehicle
     puts "You are now going #{self.speed}"
   end
   
   def shut_the_car_off
     self.speed = 0
-    puts "Your car is now off and speed is 0"
+    puts "Your #{self.model} is now off and speed is 0"
+  end
+  
+  def self.find_mileage(gallons, miles)
+    puts "#{miles / gallons} per gallon"
+  end
+
+  def number_of_vehicles?
+    puts @@number_of_vehicles
+  end
+  
+  def to_s
+    "My #{self.model} is a #{self.color}, #{self.year}, #{self.model}."
   end
   
   def spray_paint
@@ -36,32 +60,58 @@ attr_reader :year
     puts "Rattle, rattle, rattle.  What color is your spray paint?"
     new_color = gets.chomp
     self.color = new_color
-    puts "Your car is now a shiny, bright #{self.color}"
+    puts "Your #{self.model} is now a shiny, bright #{self.color}"
   end
   
-  def self.find_mileage(gallons, miles)
-    puts "#{miles / gallons} per gallon"
+  def age
+    puts "Your #{self.model} is #{find_age} years old"
   end
   
-  def to_s
-    "My car is a #{self.color}, #{self.year}, #{self.model}."
-  end
-  
-
+  private
+    
+    def find_age
+      age = Time.now.year - self.year.to_i
+    end
     
 end
 
-fat_kiki = MyCar.new('2014', 'Black', 'Kia Soul')
+class MyCar < Vehicle
+  VEHICLETYPE = "Car"
 
-fat_kiki.speed_up(55)
-fat_kiki.slow_down(30)
-fat_kiki.slow_down(50)
-fat_kiki.speed_up(10)
-fat_kiki.shut_the_car_off
-fat_kiki.speed_up(55)
-MyCar.find_mileage(5, 94)
+  def initialize(y, c, m)
+    super(y, c, m)
+  end
+  
+  
+end
 
-fat_kiki.spray_paint
+class MyTruck < Vehicle
+  include Haulable
+  VEHICLETYPE = "Truck"
+  
+  def intialize(y, c, m)
+    super(y, c, m)
+  end
+  
+end
 
-puts fat_kiki
+fat_kiki = MyCar.new('2012', 'Black', 'Kia Soul')
+
+fat_kiki.age
+
+# fat_kiki.speed_up(55)
+# fat_kiki.slow_down(30)
+# fat_kiki.slow_down(50)
+# fat_kiki.speed_up(10)
+# fat_kiki.shut_the_car_off
+# fat_kiki.speed_up(55)
+# MyCar.find_mileage(5, 94)
+
+# fat_kiki.spray_paint
+# truck = MyTruck.new('2000', 'silver', 'Ford F150')
+# puts MyCar.ancestors
+# puts MyTruck.ancestors
+
+# puts fat_kiki
+# fat_kiki.number_of_vehicles?
 
